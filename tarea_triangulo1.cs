7a44f01e61f847e2294fa469075e2d938cc4d818
deceptibot5a <45839302+deceptibot5a@users.noreply.github.com>
2020-01-24 11:00:40 -0500

    Add files via upload

---
+++ tarea_triangulo1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Entrada catetos + salida hipotenusa
+            Console.Write("Ingrese el cateto 'y': ");
+            double y = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese el cateto 'z': ");
+            int z = int.Parse(Console.ReadLine());
+            double t = Math.Sqrt(Math.Pow(z, 2) + Math.Pow(y, 2));
+            Console.WriteLine("La hipotenusa es: " + t);
+
+            //Cálculo y conversión ángulos
+            double aG = Math.Asin(y / t);
+            double cG = Math.Asin(z / t);
+            double a = aG * (180 / Math.PI);
+            double c = cG * (180 / Math.PI);
+
+            Console.WriteLine("Los ángulos son: 90° " + a + "° y " + c + "°");
+
+            Console.Read();
+        }
+    }
+}
