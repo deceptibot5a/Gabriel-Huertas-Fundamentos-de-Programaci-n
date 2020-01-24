c15ed7e3ef1727831b9082ff9daf4d972e8c8a22
deceptibot5a <45839302+deceptibot5a@users.noreply.github.com>
2020-01-24 00:14:34 -0500

    Add files via upload

---
+++ bin2dec.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer digito: ");
+            double b0 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo digito: ");
+            double b1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el tercer digito: ");
+            double b2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el cuarto digito: ");
+            double b3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el ultimo digito: ");
+            double b4 = double.Parse(Console.ReadLine());
+            double r0 = b0 * Math.Pow(2, 4);
+            double r1 = b1 * Math.Pow(2, 3);
+            double r2 = b2 * Math.Pow(2, 2);
+            double r3 = b3 * Math.Pow(2, 1);
+            double r4 = b4 * Math.Pow(2, 0);
+            double resultado = r0 + r1 + r2 + r3 + r4;
+            Console.WriteLine("El numero en decimal es " + resultado);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ coordenadas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un valor en x: ");
+            double x1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese un valor en y: ");
+            double y1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese otro valor en x: ");
+            double x2 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese otro valor en y: ");
+            double y2 = int.Parse(Console.ReadLine());
+            double pendiente = ((y2 - y1) / (x2 - x1));
+            double intercepto = y1 - (pendiente * x1);
+            double distancia = Math.Sqrt(Math.Pow(y2 - y1, 2) + Math.Pow(x2 - x1, 2));
+            Console.WriteLine("La pendiente es: " + pendiente + ", el intercepto es: " + intercepto + " y la distancia es: " + distancia);
+
+        }
+    }
+}
