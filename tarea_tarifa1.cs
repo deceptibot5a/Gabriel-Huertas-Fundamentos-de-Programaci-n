60f1312106f56e95949eee2aff309c933549ab90
deceptibot5a <45839302+deceptibot5a@users.noreply.github.com>
2020-01-29 23:38:12 -0500

    Add files via upload

---
+++ tarea_tarifa1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su salario: ");
+            int salario = int.Parse(Console.ReadLine());
+
+            int minimo = 877803;
+            int usuario = salario / minimo;
+
+
+            if (usuario < 2)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa A");
+
+            }
+            else if (2 <= usuario && usuario < 4)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa B");
+
+            }
+            else
+            {
+                Console.WriteLine("Usted debe pagar la tarifa C");
+            }
+            
+
+
+            Console.Read();
+        }
+    }
+}
