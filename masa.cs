90b687dd57b702c860f89b37deac98c6bb9adeef
deceptibot5a <45839302+deceptibot5a@users.noreply.github.com>
2020-01-29 23:14:02 -0500

    Add files via upload

---
+++ masa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su peso en kilogramos: ");
+            double peso = double.Parse(Console.ReadLine());
+            Console.Write("Ingrese su estatura en metros: ");
+            double estatura = double.Parse(Console.ReadLine());
+
+            double imc = peso / (Math.Pow(estatura, 2));
+
+
+            if (imc < 18.5)
+            {
+                Console.WriteLine("Usted está bajo de peso");
+                Console.Write("Su indice de masa corporal es: " + imc);
+            }
+            else if (18.6 <= imc && imc <= 25)
+            {
+                Console.WriteLine("Usted tiene peso normal");
+                Console.Write("Su indice de masa corporal es: " + imc);
+            }
+            else if (25 < imc && imc <= 35)
+            {
+                Console.WriteLine("Usted tiene sobrepeso");
+                Console.Write("Su indice de masa corporal es: " + imc);
+            }
+            else
+            {
+                Console.WriteLine("Usted tiene obesidad");
+                Console.Write("Su indice de masa corporal es: " + imc);
+            }
+
+
+            Console.Read();
+        }
+    }
+}
