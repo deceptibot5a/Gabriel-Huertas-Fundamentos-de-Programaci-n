3e1e0f60f64ba937dfa3b1978a0b62a3a39740f6
deceptibot5a <45839302+deceptibot5a@users.noreply.github.com>
2020-01-29 23:56:09 -0500

    Add files via upload

---
+++ tarea_tarifa2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su salario: ");
+            int salario = int.Parse(Console.ReadLine());
+
+            int minimo = 877803;
+            int usuario = salario / minimo;
+
+
+            if (usuario < 2)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa A");
+                Console.WriteLine("Valor de cuota moderadora: $3400");
+
+            }
+            else if (2 <= usuario && usuario < 5)
+            {
+                Console.WriteLine("Usted debe pagar la tarifa B");
+                Console.WriteLine("Valor de cuota moderadora: $13500");
+
+            }
+            else
+            {
+                Console.WriteLine("Usted debe pagar la tarifa C");
+                Console.WriteLine("Valor de cuota moderadora: $35600");
+            }
+            
+
+
+            Console.Read();
+        }
+    }
+}
