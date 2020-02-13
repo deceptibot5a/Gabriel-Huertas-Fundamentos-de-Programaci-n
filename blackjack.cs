b752b9c14ec112a9f2b707c9116e5fb20b027226
deceptibot5a <45839302+deceptibot5a@users.noreply.github.com>
2020-02-13 01:00:55 -0500

    Add files via upload

---
+++ blackjack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random aleatorio = new Random();
+            int carta = aleatorio.Next(1, 11), segunda = aleatorio.Next(1, 11), total = carta + segunda;
+            Console.WriteLine("Sus cartas son: " + carta + " y " + segunda);
+            Console.WriteLine("Total: " + total);
+            Console.WriteLine("Desea continuar? (s/n): ");
+            string respuesta = Console.ReadLine();
+
+
+            while (respuesta == "s" && total < 21)
+            {
+                carta = aleatorio.Next(1, 11);
+                total += carta;
+                if (total < 21)
+                {
+                    Console.WriteLine("Su carta fue: " + carta);
+                    Console.WriteLine("Total: " + total);
+                    Console.WriteLine("¿Desea continuar? (s/n): ");
+                    respuesta = Console.ReadLine();
+                }
+                else if (total == 21)
+                {
+                    Console.WriteLine("Su carta fue: " + carta);
+                    Console.WriteLine("Has ganado!");
+                }
+                else
+                {
+                    Console.WriteLine("Su carta fue: " + carta);
+                    Console.WriteLine("Eliminado");
+                }
+                
+            }
+            Console.WriteLine("Su total fue: " + total + " puntos");
+            Console.WriteLine("Gracias por participar");
+            Console.Read();
+
+
+        }
+    }
+}
